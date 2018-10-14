7595069c8c4b5d13175a3361d4f0b9c546e06117
Scott Macmillan <CommanderTso@users.noreply.github.com>
2018-10-14 04:51:33 -0400

    Add initial db schema

---
+++ create_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+db_file = 'thermal_printer_db.sqlite'
+
+conn = sqlite3.connect(db_file)
+c = conn.cursor()
+
+c.execute('CREATE TABLE IF NOT EXISTS users ( \
+	id INTEGER PRIMARY KEY NOT NULL, \
+	user_name TEXT NOT NULL, \
+	password TEXT NOT NULL)')
+
+conn.commit()
+conn.close()
