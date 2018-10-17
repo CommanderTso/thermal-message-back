b0bf9485ba03be6eabdd9d38676c463ae50c1c3f
Scott Macmillan <CommanderTso@users.noreply.github.com>
2018-10-17 12:04:23 -0400

    Work-in-progress checkin for webServer.py

--- webServer.py
+++ webServer.py
@@ -1,15 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-@app.route('/send-message', methods=['GET'])
-def message_get():
-    return 'message -> get\n'
+@app.route('/', methods=['GET'])
+def index():
+    if False: # if not valid cookie
+        # reroute to login endpoint
+        return
+    else:
+        return redirect(url_for('message_post'))
 
-@app.route('/message', methods=['POST'])
+@app.route('/message-post', methods=['POST', 'GET'])
 def message_post():
-    post_data = request.get_json()
-    print(post_data)
-    return "success"
+    if request.method == 'POST':
+        post_data = request.get_json()
+        print(post_data)
+        return "success"
+    elif request.method == 'GET':
+        return "message posting page!"
+    else:
+        abort(405)  # Method Not Allowed
