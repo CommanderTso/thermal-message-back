65ffa508ef1ec154e1bf6dc5596c2b83d435d365
Scott Macmillan <CommanderTso@users.noreply.github.com>
2018-11-08 17:01:07 -0500

    Add bcrypt, SQLite db setup, redo hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+from flask.ext.bcrypt import Bcrypt
 
-@app.route('/message', methods=['GET'])
-def message_get():
-    return 'message -> get\n'
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+# @app.route('/message', methods=['GET'])
+# def message_get():
+#     return 'message -> get\n'
 
 @app.route('/message', methods=['POST'])
 def message_post():
-    return 'message -> post\n'
+    post_data = request.get_json()
+    print(post_data)
+    return "success"
