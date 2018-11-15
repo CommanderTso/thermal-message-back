22e508ae0a21c3adf0e21d3fbf03ba1498fbb9f9
Scott Macmillan <CommanderTso@users.noreply.github.com>
2018-11-15 16:12:03 -0500

    Add front end template
    - add template for message submission
    - handle response on backend

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
-from flask import Flask, request
-from flask.ext.bcrypt import Bcrypt
+from flask import Flask, request, render_template
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-# @app.route('/message', methods=['GET'])
-# def message_get():
-#     return 'message -> get\n'
+@app.route('/', methods=['GET'])
+def home_page():
+    return render_template('index.html')
 
 @app.route('/message', methods=['POST'])
 def message_post():
-    post_data = request.get_json()
-    print(post_data)
-    return "success"
+    message_to_print = request.form['user_message']
+    print(f"form: {message_to_print}")
+    return "POST /message success"
