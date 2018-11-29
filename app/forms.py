87ea1dc7a8f0a9b94e7db6ace9a3334cd954c94b
Scott Macmillan <CommanderTso@users.noreply.github.com>
2018-11-29 07:22:27 -0500

    Change Message field to TextArea

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 class SubmitMessageForm(FlaskForm):
-    message = TextField('Message', validators=[DataRequired()])
+    message = TextAreaField('Message', validators=[DataRequired()])
     submit = SubmitField('Send Message')
