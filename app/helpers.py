187159ed755bf3d6653db3146c966f8ebfe5b5ae
Scott Macmillan <CommanderTso@users.noreply.github.com>
2018-12-06 07:14:20 -0500

    Document expected data for helper

--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,6 @@
 def parse_login_error(errors):
+    # example WTForm validation error dict:
+    # {'username': ['Invalid email address.'], 'password': ['This field is required.']}
     if errors is None:
         return None
     elif 'username' in errors and errors['username'][0] == "Invalid email address.":
